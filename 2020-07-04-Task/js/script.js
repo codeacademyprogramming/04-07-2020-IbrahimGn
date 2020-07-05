4cf7f79c21d1949e420ef8c876b4e9f9d01e3956
Ibrahim Nazarov <55439563+IbrahimGn@users.noreply.github.com>
2020-07-05 09:59:15 +0400

    2nd commit

--- 2020-07-04-Task/js/script.js
+++ 2020-07-04-Task/js/script.js
@@ -4,14 +4,14 @@
 
  or
 */
-function sum(arr) {
-    let result = 0;
-    for (const element of arr) {
-        result += element;
-    }
-    return result;
-}
-console.log(sum([1, 7, 6, -9]));
+// function sum(arr) {
+//     let result = 0;
+//     for (const element of arr) {
+//         result += element;
+//     }
+//     return result;
+// }
+// console.log(sum([1, 7, 6, -9]));
 
 
 
@@ -30,26 +30,27 @@ console.log(roundUpIntMultiFive(-11));
 */
 
 // ----4) Write a JavaScript function to get difference between given birth date and current date. It should return number of years, months and days.
+
 /*
-    function showTimeFromMyBirth(day, month, year) {
-        let date = new Date();
-        let myBirth = new Date(year, month - 1, day);
-        let getYear = date.getFullYear() - myBirth.getFullYear();
-        let getMonth = date.getMonth() - myBirth.getMonth();
-        let getDay = date.getDate() - myBirth.getDate();
-        if (date < myBirth) {
-            console.log(`You don't birth!!!`);
-            return;
-        }
-        if (getMonth < 0) {
-            getMonth = 12 + getMonth;
-            getYear = getYear - 1;
-        }
-        if (getDay < 0) {
-            getDay = 31 + getDay;
-            getMonth = getMonth - 1;
-        }
-        console.log(`year: ${getYear}, month: ${getMonth}, day: ${getDay}`)
+function showTimeFromMyBirth(day, month, year) {
+    let date = new Date();
+    let myBirth = new Date(Math.abs(year), Math.abs(month - 1), Math.abs(day));
+    let getYear = date.getFullYear() - myBirth.getFullYear();
+    let getMonth = date.getMonth() - myBirth.getMonth();
+    let getDay = date.getDate() - myBirth.getDate();
+    if (date < myBirth) {
+        console.log(`You don't birth!!!`);
+        return;
     }
-    showTimeFromMyBirth(15, 9, 2019);
-    */
+    if (getMonth < 0) {
+        getMonth = 12 + getMonth;
+        getYear = getYear - 1;
+    }
+    if (getDay < 0) {
+        getDay = 31 + getDay;
+        getMonth = getMonth - 1;
+    }
+    console.log(`year: ${getYear}, month: ${getMonth}, day: ${getDay}`)
+}
+showTimeFromMyBirth(15, 9, -2019);
+*/
